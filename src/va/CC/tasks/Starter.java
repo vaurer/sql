9e970680b621e2120833732e179dca1e1d1682d8
vaurer <vaurer@gmail.com>
2020-06-22 21:50:23 +0200

    first

--- src/va/CC/tasks/Starter.java
+++ src/va/CC/tasks/Starter.java
@@ -3,7 +3,7 @@ package va.CC.tasks;
 public class Starter {
     public static void main(String[] args) {
         TaskManager taskManager = new TaskManager();
-//        taskManager.readInput();
+//        taskManager.readInput(); //
         taskManager.printAllTasks();
         TaskDAO taskDAO = new TaskDAO();
         taskDAO.getAllTasks();
